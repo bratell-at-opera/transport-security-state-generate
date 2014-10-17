7c608e785bc51050db5e06c4dca84bc2d4bc09e0
Daniel Bratell <bratell@opera.com>
2014-10-17 17:50:47 +0200

    Add license text crediting Chromium Authors (ie. I :-) ).

--- x509decode.py
+++ x509decode.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 """Minimal x509 decoder that extracts exactly what
 transport_security_state_static_generate needs and nothing else."""
 
